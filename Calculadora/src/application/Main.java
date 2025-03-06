fe8b30be20d1a06634c316cbd5fc6451dfdb9d79
Pedro-Pires-fag <pebpires@minha.fag.edu.br>
2025-03-06 19:28:27 -0300

    segundo commit

--- Calculadora/src/application/Main.java
+++ Calculadora/src/application/Main.java
@@ -1,8 +1,53 @@
 package application;
 
+import entities.Calculadora;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Calculadora calculadora = new Calculadora();
+        Scanner scan = new Scanner(System.in);
+        menu();
+        System.out.println("Escolha uma opcao: ");
+        int option = scan.nextInt();
 
+        while (option != 3){
+            switch (option){
+                case 1:
+                {
+                    System.out.println("Digite o valor do produto:");
+                    double produto = scan.nextDouble();
+                    System.out.println("Digite a quantidade do produto:");
+                    int quantidade = scan.nextInt();
+                    calculadora = new Calculadora(produto, quantidade);
+                    System.out.println("Valor total da compra: " + calculadora.totalValue());
+                    break;
+                }
+                case 2:
+                {
+                    System.out.println("Digite o valor do pagamento: ");
+                    double pagamento = scan.nextDouble();
+                    System.out.println("Troco: " + calculadora.change(calculadora.totalValue(), pagamento));
+                    break;
+                }
+                default:
+                {
+                    break;
+                }
+            }
+            menu();
+            System.out.println("Escolha uma opcao: ");
+            option = scan.nextInt();
+        }
+
+    }
+
+
+    public static void menu ()
+    {
+        System.out.println(
+                "[1] - Calcular Preço Total \n" + "[2] - Calcular Troco\n" + "[3] - Sair  \n"  );
     }
 }
--- Calculadora/src/entities/Calculadora.java
+++ Calculadora/src/entities/Calculadora.java
@@ -5,22 +5,32 @@ public class Calculadora {
     private int quantity;
     private double cost;
 
+    public Calculadora (){
+
+    }
     public Calculadora(double cost, int quantity) {
         this.cost = cost;
         this.quantity = quantity;
     }
 
-    public Double totalValue(int quantity, Double cost)
+    public Double totalValue()
     {
         return quantity * cost;
     }
 
     public Double change(Double totalValue, Double payment){
-        Double result= 0.0;
+        double result= 0.0;
         if (payment > totalValue){
             result = payment - totalValue;
         }
         return result;
     }
 
+    public double getCost() {
+        return cost;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
